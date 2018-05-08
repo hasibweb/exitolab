f4bd1f49099079dfaf99aa13bc0175b39a6436d8
Hasibur <hasibonline2@gmail.com>
2018-05-08 17:09:52 +0600

    sticky

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,12 @@
     // Window Load Function
     $(window).on('load', function () {
         preloaderSetup();
-        heroSlider();
 
     });
 
     // Document Ready Function
     $(document).ready(function () {
         animations();
-        mobileMenu();
 
     });
 
@@ -30,7 +28,16 @@
     $(window).on('resize', function () {});
 
     // Window Scroll Function
-    $(window).on('scroll', function () {});
+    $(window).on('scroll', function () {
+        var top = $(window).scrollTop();
+
+        if (top >= 100) {
+            $('.page-header .navbar').addClass('sticky');
+        } else {
+            $('.page-header .navbar').removeClass('sticky');
+        }
+
+    });
 
     // ========================== preloaderSetup ==========================
     function preloaderSetup() {
@@ -42,107 +49,7 @@
 
     // ========================== Animations ==========================
     function animations() {
-        AOS.init({
-            offset: 50
-        });
-
-    }
-
-    // ========================== Mobile Responsive Menu ==========================
-    function mobileMenu() {
-        var menu = $('.page-sidebar').html();
-        var mobMenu = '<div class="mobile-menu-slide py-4">' + menu + '</div>';
-        $('body').prepend(mobMenu);
-
-        $('.mobile-menu-icon').on('click', function (e) {
-            e.stopPropagation();
-            e.preventDefault();
-            $('.mobile-menu-slide').addClass('show');
-        })
-
-        // Hide on click outside
-        $(document).on('click', function (e) {
-            if (!(e.target.closest('.mobile-menu-slide'))) {
-                $('.mobile-menu-slide').removeClass('show');
-            }
-        })
-
-    }
-    // ========================== HeroSlider ==========================
-    function heroSlider() {
-        var slider = $('.hero-slider-init');
-        var sliderNav = $('.hero-slider-nav');
-        var count = $('.hero-slider-nav li').length;
-        slider.slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            asNavFor: sliderNav,
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 5000
-        })
-        sliderNav.slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            arrows: false,
-            asNavFor: slider,
-            dots: false,
-            centerMode: true,
-            focusOnSelect: true,
-            vertical: true,
-            adaptiveHeight: true
-        })
-
-        // Add Animation Globaly
-        slider
-            .find('[data-animation]')
-            .each(function () {
-                var animation = $(this).data('animation');
-                $(this).addClass(animation + ' animated')
-            })
-
-        // Animation Duration
-        slider
-            .find('[data-duration]')
-            .each(function () {
-                var duration = $(this).data('duration');
-                $(this).css('animation-duration', duration);
-            })
-
-        // Animation Dealy
-        slider
-            .find('[data-delay]')
-            .each(function () {
-                var delay = $(this).data('delay');
-                $(this).css('animation-delay', delay);
-            })
-
-        // Before Change
-        slider.on('beforeChange', function (event) {
-            var layer = $(event.target).find('[data-animation]');
-
-            layer.each(function () {
-                var animation = $(this).data('animation');
-                $(this)
-                    .removeClass(animation + ' animated')
-                    .css('opacity', '0');
-            })
-        })
-        // After Change
-        slider.on('afterChange', function (event) {
-            var layer = $(event.target)
-                .find('.slick-slide.slick-active')
-                .find('[data-animation]');
-
-            layer.each(function () {
-                var animation = $(this).data('animation');
-                $(this)
-                    .addClass(animation + ' animated')
-                    .css('opacity', '1');
-            })
-        })
+        AOS.init({offset: 50});
 
     }
 
